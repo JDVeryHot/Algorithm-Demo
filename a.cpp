f7d40d9b85c870a72b22ae15684a36ee26a6a312
JDVeryHot <53112285+JDVeryHot@users.noreply.github.com>
2019-07-20 16:49:12 +0800

    Update a.cpp

--- a.cpp
+++ a.cpp
@@ -1,1 +1,27 @@
-aaa
+unsigned int  subStr(const string &parentStr,const string &subStr){
+    unsigned int num = 0;
+    const char *pStr   = parentStr.c_str();
+    const char *pSub   = subStr.c_str();
+   
+    for (int i = 0,s = 0,last = 0; i<parentStr.length(); i++) {
+        if (s == 0) {
+            last = i;
+            if (parentStr.length() - i < subStr.length())break;
+        }
+        
+        if (pStr[i] != pSub[s]) {
+            s = 0;
+            i = last;
+            continue;
+        }
+        
+        if (s == subStr.length()-1) {
+            num ++;
+            s = 0;
+            continue;
+        }
+        s ++;
+    }
+    
+    return num;
+}
